aaaf5e1f246f9fcbef4321affdc73372e77d28f3
鎌田　拓臣 <yuuteayuu335dat@gmail.com>
2020-09-10 09:32:54 +0900

    課題

--- v0910/Form1.cs
+++ v0910/Form1.cs
@@ -12,9 +12,35 @@ namespace v0910
 {
     public partial class Form1 : Form
     {
+        static Random rand = new Random();
+
+        int[] vx = new int[6];
+        int[] vy = new int[6];
+        Label[] labels = new Label[100];
         public Form1()
         {
             InitializeComponent();
+            for (int i = 0; i < 6; i++)
+            {
+                vx[i] = rand.Next(-15, 16);
+                vy[i] = rand.Next(-15, 16);
+                labels[i] = new Label();
+                labels[i].AutoSize = true;
+                labels[i].Text = "★";
+                Controls.Add(labels[i]);
+                labels[i].Left = rand.Next(ClientSize.Width - labels[i].Width);
+                labels[i].Top = rand.Next(ClientSize.Height - labels[i].Height);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
         }
     }
 }
